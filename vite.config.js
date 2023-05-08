60fe09c56f5c4acfb1d05673a90cd76c0c54871f
Thoriq Hanif <thoriqmuh123.alkapro@gmail.com>
2023-05-08 08:37:48 +0700

    tes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/thoriq-hanif/'
+  base: '/thoriq-hanif/',
+  plugins: [react()]
 })
